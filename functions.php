cd2232da22dfd82e09ae5807fa18db01c1f1baf8
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2018-09-16 19:33:57 +1000

    build 1.4

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function joelkrause() {
 	wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
 	wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.3.1/css/all.css');
 	wp_enqueue_style( 'joelkrause-style', get_stylesheet_uri() );
-	wp_enqueue_style( "style", get_stylesheet_directory_uri()."/library/css/main.css?v=1.0", "all");
+	wp_enqueue_style( "style", get_stylesheet_directory_uri()."/library/css/main.css", "all");
 	
 }
 add_action( 'wp_enqueue_scripts', 'joelkrause' );
